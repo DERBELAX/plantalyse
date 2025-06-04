a6759a53227d4a585cd0327c8536a2766be86d53
DERBELAX <164332911+DERBELAX@users.noreply.github.com>
2025-06-04 07:52:37 +0200

    first commit

---
+++ src/main/java/com/example/plantalysBackend/PlantalysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantalysBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantalysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantalysBackendApplication.class, args);
+	}
+
+}
