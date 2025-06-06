2b8ad0fb6dcc80a5502a3320e10de5f417161bad
DERBELAX <164332911+DERBELAX@users.noreply.github.com>
2025-06-06 08:32:59 +0200

    add entity plant and category

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -1,5 +1,49 @@
 package model;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
 public class Category {
-
+	  
+	@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id_category;
+	    private String name;
+	    @OneToMany(mappedBy = "categories")
+	    private List<Plant> plants;
+		public Category(Long id_category, String name, List<Plant> plants) {
+			super();
+			this.id_category = id_category;
+			this.name = name;
+			this.plants = plants;
+		}
+		public Category() {
+			super();
+		}
+	    
+		public Long getId_category() {
+			return id_category;
+		}
+		public void setId_category(Long id_category) {
+			this.id_category = id_category;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public List<Plant> getPlants() {
+			return plants;
+		}
+		public void setPlants(List<Plant> plants) {
+			this.plants = plants;
+		}
 }
--- src/main/java/model/Plant.java
+++ src/main/java/model/Plant.java
@@ -1,5 +1,111 @@
 package model;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+
+
+@Entity
+@Table(name = "plant")
 public class Plant {
+	 
+
+	@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id_plante;
+
+	    @Column(columnDefinition = "TEXT")
+	    private String description;
+
+	    private String image;
+	    private Double price;
+	    private String name;
+	    private Integer stock;
+
+	    @ManyToOne
+	    @JoinColumn(name = "id_category") 
+	    private Category category;
+
+		public Plant(Long id_plante, String description, String image, Double price, String name, Integer stock,
+				Category category) {
+			super();
+			this.id_plante = id_plante;
+			this.description = description;
+			this.image = image;
+			this.price = price;
+			this.name = name;
+			this.stock = stock;
+			this.category = category;
+		}
+
+		public Plant() {
+			super();
+		}
+		
+		
+		public Long getId_plante() {
+			return id_plante;
+		}
+
+		public void setId_plante(Long id_plante) {
+			this.id_plante = id_plante;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getImage() {
+			return image;
+		}
+
+		public void setImage(String image) {
+			this.image = image;
+		}
+
+		public Double getPrice() {
+			return price;
+		}
+
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Integer getStock() {
+			return stock;
+		}
+
+		public void setStock(Integer stock) {
+			this.stock = stock;
+		}
+
+		public Category getCategory() {
+			return category;
+		}
+
+		public void setCategory(Category category) {
+			this.category = category;
+		}
+
 
 }
